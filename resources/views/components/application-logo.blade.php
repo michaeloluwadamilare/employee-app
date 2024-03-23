0fb136c295a3d04802f5c63fd4fd16e3036d96a1
Oluwadamilare Michael <63959057+michaeloluwadamilare@users.noreply.github.com>
2024-03-23 17:11:22 +0100

    Update application-logo.blade.php

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,5 +1,5 @@
 <div class="d-flex align-items-center justify-content-between">
-      <a href="index.html" class="logo d-flex align-items-center">
+      <a href="/" class="logo d-flex align-items-center">
         <img src="{{ asset('assets/img/mint_logo_white.png') }}" style="width:100px" alt="">
       </a>
       <i class="bi bi-list toggle-sidebar-btn"></i>
