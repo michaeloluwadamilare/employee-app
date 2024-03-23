a5825dc89f213892d4c8b323b94d9ffeb7ce2a4d
Oluwadamilare Michael <63959057+michaeloluwadamilare@users.noreply.github.com>
2024-03-23 17:04:55 +0100

    Update navigation.blade.php

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -2,8 +2,8 @@
   <header id="header" class="header fixed-top d-flex align-items-center">
 
     <div class="d-flex align-items-center justify-content-between">
-      <a href="index.html" class="logo d-flex align-items-center">
-        <img src="assets/img/mint_vector_black.png" alt="">
+      <a href="{{'dashboard'}}" class="logo d-flex align-items-center">
+        <img src="{{asset('assets/img/mint_vector_black.png') }}" alt="">
         <span class="d-none d-lg-block">MINT BAR</span>
       </a>
       <i class="bi bi-list toggle-sidebar-btn"></i>
