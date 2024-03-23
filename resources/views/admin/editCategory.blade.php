f8660329dfe6948de2432d4a0164759eadab5477
DevBasito <vampbaxx@gmail.com>
2024-03-23 18:48:01 +0100

    modal-lg
    modal-lg

--- resources/views/admin/editCategory.blade.php
+++ resources/views/admin/editCategory.blade.php
@@ -1,5 +1,5 @@
 <div class="modal fade" id="editCategoryModal{{$v_category->id}}" >
-  <div class="modal-dialog modal-xl">
+  <div class="modal-dialog modal-lg">
     <div class="modal-content">
       <div class="modal-header">
         <h5 class="modal-title" >Edit Category</h5>
--- resources/views/admin/editMenu.blade.php
+++ resources/views/admin/editMenu.blade.php
@@ -1,5 +1,5 @@
 <div class="modal fade" id="editMenuModal{{$list->id}}" >
-  <div class="modal-dialog modal-xl">
+  <div class="modal-dialog modal-lg">
     <div class="modal-content">
       <div class="modal-header">
         <h5 class="modal-title" >Edit Menu</h5>
