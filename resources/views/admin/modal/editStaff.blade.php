2e3216f93412ac04936a3b9831e32a996ff93859
Oluwadamilare Michael <63959057+michaeloluwadamilare@users.noreply.github.com>
2024-03-24 19:57:47 +0100

    Update editStaff.blade.php

--- resources/views/admin/modal/editStaff.blade.php
+++ resources/views/admin/modal/editStaff.blade.php
@@ -6,7 +6,7 @@
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
-        <form action="{{ route('menu-lists.store') }}" method="POST">
+        <form action="{{ route('staff-lists.store') }}" method="POST">
 
           <div class="container mt-2">
             @csrf
