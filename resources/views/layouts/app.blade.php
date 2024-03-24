2a3687f16fc550b58a6e82f0b166bbd64031b209
DevBasito <vampbaxx@gmail.com>
2024-03-24 14:40:01 +0100

    create table
    create role and staff table

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{config('app.name'),'-Admin'}}</title>
+        <title>{{config('app.name','-Admin')}}</title>
 
         <link href="assets/img/favicon.png" rel="icon">
         <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
