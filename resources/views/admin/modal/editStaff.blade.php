76b325ea6b82da1611c6ff0044afee6a54823286
Oluwadamilare Michael <63959057+michaeloluwadamilare@users.noreply.github.com>
2024-03-24 20:04:16 +0100

    Update editStaff.blade.php

--- resources/views/admin/modal/editStaff.blade.php
+++ resources/views/admin/modal/editStaff.blade.php
@@ -6,11 +6,11 @@
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
-        <form action="{{ route('staff-lists.store') }}" method="POST">
+        <form action="{{ route('staff-lists.update', ['id' => $staff->id]) }}" method="POST">
 
           <div class="container mt-2">
-            @csrf
             @method('PUT')
+            @csrf
 
             <div class="mb-3">
               <label for="name">Name</label>
