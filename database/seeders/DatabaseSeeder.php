cff2c82b0d5d76d9df6fc058407d0626ea735d88
Oluwadamilare Michael <63959057+michaeloluwadamilare@users.noreply.github.com>
2024-03-24 18:46:14 +0100

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -36,7 +36,7 @@ class DatabaseSeeder extends Seeder
             'password' => '$2y$12$5xfeJ4tfms2SxksIw0FjoucmHHIjpwLwhIaBjRqjPcMfJqutT2m/6',
             'status' => 'Active',
             'job_description' => 'Administrator',
-            'role_id' => '1',
+            'roles_id' => '1',
         ]);
         DB::table('users')->insert([
             'name' => 'John Doe',
@@ -44,7 +44,7 @@ class DatabaseSeeder extends Seeder
             'password' => '$2y$12$5xfeJ4tfms2SxksIw0FjoucmHHIjpwLwhIaBjRqjPcMfJqutT2m/6',
             'status' => 'Active',
             'job_description' => 'Waiter',
-            'role_id' => '2',
+            'roles_id' => '2',
         ]);
 
         DB::table('categories')->insert([
